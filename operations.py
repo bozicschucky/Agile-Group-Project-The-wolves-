f6bdc041c6bd6f8c802af8f29c8a195bb98d8b62
Besufekd <besufekadsm@gmail.com>
2018-06-01 11:18:12 +0300

    operations

--- operations.py
+++ operations.py
@@ -1,39 +1,41 @@
-import store
 
-class operations(store):
-
-    #Edit Comment
+class operations():
+    users = []
+    comments =[]
+    #Edit Commenti
     def edit_comment(self,user,comment):
         if "admin"==user.role:
-            for int i=0;i<len(comments;i++):
-                if comments[i]['id'] = comment['id']:
-                    comment[i]=comment
-                    print "You have Succesfully edited the comment."
+            i=0
+            for com in comments:
+                if com['id'] == comment['id']:
+                    comments[i]=comment
+                    return "You have Succesfully edited the comment."
                 else:
-                    print "The comment you selected doesnt Exist"
+                    return "The comment you selected doesnt Exist"
         else:
-            print "You are not authorized to edit this comment"
+            return "You are not authorized to edit this comment"
 
     #Delete Comment
     def delete_comment(self,user,comment):
         if "admin"==user.role or "moderator"==user.role:
-            for int i=0;i<len(comments;i++):
-                if comments[i]['id'] = comment['id']:
+            i=0
+            for com in comments:
+                if com['id'] == comment['id']:
                     del comments[i]
-                    print "You have Succesfully deleted the comment."
+                    return "You have Succesfully deleted the comment."
                 else:
-                    print "The comment you selected doesnt Exist"
+                    return "The comment you selected doesnt Exist"
         else:
-            print "You are not authorized to edit this comment"
+            return "You are not authorized to edit this comment"
 
     #Create Comment
     def create_comment(self,comment):
         comments.append(comment)
 
     def get_comment_by_id(self,id):
-        for int i=0;i<len(comments;i++):
-            if comments[i]['id'] = comment['id']:
-                return comment[i]
-        print "The comment you selected doesnt Exist"
-            return None
+        i=0
+        for com in comments:
+            if com['id'] == comment['id']:
+                return comments[i]
+        return None
 
--- store.py
+++ store.py
@@ -1,3 +1,3 @@
 
-self.users = []
-self.comments =[]
+users = []
+comments =[]
